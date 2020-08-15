832fbdd779a16c79cf48ff271e51e240e0300a01
xiaoshi <55978649+xs314@users.noreply.github.com>
2020-08-15 14:45:54 +0800

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
+    'm785759550-19c5db180c5968c048dc991d',
     'm784490063-7b5da437e7f1e0d67613714d',
     'm784497419-de55aa09902ccb3ab22d548a',
     'm784496436-71a4bf7b1e3bdf7756be131b',
@@ -28,7 +28,7 @@ window.Config = {
   Navi: [
     {
       text: 'Homepage',
-      url: 'https://status.org.cn/'
+      url: '/'
     },
     {
       text: 'GitHub',
